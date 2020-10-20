4d8fafefd5ed9f5260acd9b4b0b1d3ca91828c6d
SDragan15 <draganstancevic15@gmail.com>
2020-10-20 20:48:34 +0200

    Added style for settings and buttons: apply, cancel, slider for trail

---
+++ settings.js
@@ -0,0 +1,10 @@
+var settingsBtn = document.getElementById('settings_btn');
+var trailSlider = document.getElementById('trail_slider');
+
+settingsBtn.addEventListener('click', openSettings);
+
+
+
+function openSettings(){
+	console.log(trailSlider.value);
+}
